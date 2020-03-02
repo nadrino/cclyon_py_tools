d256f22f6f0f95dab2d03eadc044b1a8e3953194
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-03-02 15:35:41 +0100

    Fast commit.

--- scripts/pull_from_irods.py
+++ scripts/pull_from_irods.py
@@ -43,12 +43,42 @@ for pull_folder in parameters_dict["pull_folders"]:
 
     printout = subprocess.run(['icd', pull_folder], stdout=subprocess.PIPE)
     if "No such directory" in printout.stdout.decode('utf-8'):
-        print(toolbox.alert + "Directory \""+pull_folder+"\" has not been found.")
+        print( toolbox.alert + "Directory \""+pull_folder+"\" has not been found." )
         continue
 
-    printout = subprocess.run(['ils'], stdout=subprocess.PIPE)
-    print(printout.stdout.decode('utf-8').split('\n'))
+    if pull_folder[-1] == '/':
+        pull_folder = pull_folder[:-1]
+    head_folder = pull_folder.split('/')[-1]
+    toolbox.mkdir(head_folder)
+    os.chdir(current_working_directory + "/" + head_folder)
 
+    def recursive_download(download_path_):
+
+        printout = subprocess.run(['icd', pull_folder], stdout=subprocess.PIPE)
+        if "No such directory" in printout.stdout.decode('utf-8'):
+            return
+
+        toolbox.mkdir("")
+
+        printout = subprocess.run(['ils'], stdout=subprocess.PIPE)
+        ls_list = printout.stdout.decode('utf-8').split('\n')[1:]
+
+        for element in ls_list:
+            if "C-" in element:
+                current_dir = os.getcwd()
+                recurse_head_folder = pull_folder.split('/')[-1]
+                print(toolbox.info + "Entering " + download_path_)
+                toolbox.mkdir(recurse_head_folder)
+                os.chdir(current_working_directory + "/" + recurse_head_folder)
+                recursive_download(element)
+                os.chdir(current_dir)
+            else:
+                print(toolbox.info + "Downloading " + download_path_)
+                subprocess.run(['iget', download_path_, "./"], stdout=subprocess.PIPE)
+
+        return
+
+    recursive_download(head_folder)
 
 
 print(toolbox.red_color +
