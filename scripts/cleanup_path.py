b33f7c142fb687ce6208710b2e05a5f70211d0a7
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-03-05 10:34:17 +0100

    Fast commit.

--- scripts/cleanup_path.py
+++ scripts/cleanup_path.py
@@ -14,15 +14,19 @@ for env in env_list:
 
     cleaned_up_path_folders = list()
     for folder in path_folders:
+        if folder[0] != '/':
+            continue
         sub_folder_list = folder.split("/")
         cleaned_up_folder_list = list()
         for sub_folder in sub_folder_list:
             if sub_folder != "":
                 cleaned_up_folder_list.append(sub_folder)
-
-        cleaned_up_path_folders.append("/".join(cleaned_up_folder_list))
+        new_path_element = "/" + "/".join(cleaned_up_folder_list)
+        if new_path_element not in cleaned_up_path_folders:
+            cleaned_up_path_folders.append()
 
     new_path = ":".join(cleaned_up_path_folders)
-    os.system("export " + env + "=\"" + new_path + "\"")
+    print("export " + env + "=\"" + new_path + "\"")
+    # os.system("export " + env + "=\"" + new_path + "\"")
 
 
--- scripts/iget_recursive.py
+++ scripts/iget_recursive.py
@@ -6,6 +6,10 @@ import sys
 import os
 import subprocess
 
+# Recursion tools
+import inspect
+# sys.setrecursionlimit(1500)
+
 parameters_dict = dict()
 
 is_dry_run = False
@@ -14,11 +18,14 @@ original_working_directory = os.getcwd()
 now_time_string = toolbox.get_now_time_string()
 
 do_overwrite = False
+is_verbose_mode = False
 parameters_dict["pull_folders"] = list()
 
 
 if len(sys.argv) == 1:
     print(toolbox.warning + "--dry-run : Make all the process but don't start the run.")
+    print(toolbox.warning + "-f : Enables overwriting.")
+    print(toolbox.warning + "-v : Verbose mode.")
     print(toolbox.warning + "Example of usage : iget_recursive.py /QMULZone1/home/asg/asg2019oa/xseccovs")
     sys.exit(0)
 
@@ -34,6 +41,9 @@ for arg_id in range(len(sys.argv)):
     elif sys.argv[arg_id] == "-f":
         do_overwrite = True
         print(toolbox.warning + "Overwrite mode enabled.")
+    elif sys.argv[arg_id] == "-v":
+        is_verbose_mode = True
+        print(toolbox.warning + "Verbose mode enabled.")
     else:
         if arg_id >= 1:
             parameters_dict["pull_folders"].append(sys.argv[arg_id])
@@ -54,41 +64,59 @@ for pull_folder in parameters_dict["pull_folders"]:
 
     def recursive_download(download_folder_):
 
-        printout = subprocess.run(['icd', pull_folder], stdout=subprocess.PIPE)
+        # print(toolbox.alert + "Recursion depth : " + str(len(inspect.stack())))
+        recursive_spaces = " "*(2*(len(inspect.stack())-1))
+
+        printout = subprocess.run(['icd', download_folder_], stdout=subprocess.PIPE)
         if "No such directory" in printout.stdout.decode('utf-8'):
             return
 
-        print(toolbox.warning + "Folder : ./" + os.getcwd()[len(original_working_directory)+1:] + "/")
+        print(toolbox.warning + "./" + os.getcwd()[len(original_working_directory)+1:] + "/")
 
         printout = subprocess.run(['ils'], stdout=subprocess.PIPE)
         ls_list = printout.stdout.decode('utf-8').split('\n')[1:]
-
+        element_list = list()
         for element in ls_list:
             element = element[2:]
             if element == "":
                 continue
+            element_list.append(element)
+            print(toolbox.warning + recursive_spaces + "├─ " + element)
+
+        for element in element_list:
+
             if "C-" in element:
                 current_dir = os.getcwd()
                 recurse_head_folder = element.split('/')[-1]
-                print(toolbox.info + "Entering " + element)
+
+                # Preparing new folder
                 toolbox.mkdir(recurse_head_folder)
-                os.chdir(original_working_directory + "/" + recurse_head_folder)
-                recursive_download(element)
+                os.chdir(current_dir + "/" + recurse_head_folder)
+                recursive_download(download_folder_ + "/" + recurse_head_folder)
+
+                # Back to the original folder
                 os.chdir(current_dir)
+                subprocess.run(['icd', download_folder_], stdout=subprocess.PIPE)
             else:
                 print(toolbox.info + "Downloading " + element)
                 if os.path.exists(element):
                     if not do_overwrite:
-                        print(toolbox.warning + element + " already exists (it may have already been downloaded).")
-                        print(toolbox.warning + "Add '-f' option to allow overwrite.")
+                        if is_verbose_mode:
+                            print(toolbox.warning + element + " already exists (it may have already been downloaded).")
+                            print(toolbox.warning + "Add '-f' option to allow overwrite.")
                         continue
                     else:
+                        if is_verbose_mode:
+                            print("rm " + element)
                         os.system("rm " + element)
-                subprocess.run(['iget', element, "./"], stdout=subprocess.PIPE)
+                command_line = ['iget', element, "./"]
+                if is_verbose_mode:
+                    command_line.append("-vP")
+                    print(" ".join(['iget', element, "./"]))
+                subprocess.run(command_line)
 
-        return
 
-    recursive_download(head_folder)
+    recursive_download(pull_folder)
 
 
 print(toolbox.red_color +
