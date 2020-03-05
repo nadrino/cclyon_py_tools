97269c74a90df5a8d64b28e95ed88ef64f8d3cca
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-03-05 10:37:10 +0100

    Fast commit.

--- scripts/cleanup_path.py
+++ scripts/cleanup_path.py
@@ -26,7 +26,7 @@ for env in env_list:
             cleaned_up_path_folders.append(new_path_element)
 
     new_path = ":".join(cleaned_up_path_folders)
-    print("export " + env + "=\"" + new_path + "\"")
-    # os.system("export " + env + "=\"" + new_path + "\"")
+    # print("export " + env + "=\"" + new_path + "\"")
+    os.system("export " + env + "=\"" + new_path + "\"")
 
 
