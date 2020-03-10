28a3b77d7602d0911441071ac487dfc3ff4e0447
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-03-10 18:12:38 +0100

    Fast commit.

--- scripts/launch_command.py
+++ scripts/launch_command.py
@@ -5,11 +5,12 @@ import sys
 import os
 import time
 
-import cclyon_toolbox_lib as toolbox_lib
+import cclyon_toolbox_lib as toolbox
 
 # Checking required env variables
-toolbox_lib.get_env_variable("WORK_DIR")
-toolbox_lib.get_env_variable("RESULTS_DIR")
+toolbox.get_env_variable("WORK_DIR")
+JOBS_DIR = toolbox.get_env_variable("JOBS_DIR")
+RESULTS_DIR = toolbox.get_env_variable("RESULTS_DIR")
 
 parmeters_dict = dict()
 
@@ -21,7 +22,7 @@ mode = "job"
 command_arg_list = list()
 
 
-print(toolbox_lib.red_color + "************** Launch command starts **************" + toolbox_lib.reset_color)
+print(toolbox.red_color + "************** Launch command starts **************" + toolbox.reset_color)
 
 for arg_id in range(len(sys.argv)):
 
@@ -38,9 +39,9 @@ for arg_id in range(len(sys.argv)):
 
 
 if len(sys.argv) == 1:
-    print(toolbox_lib.warning + "-interactive : Launch script in prompt")
-    print(toolbox_lib.warning + "-debug : verbose-only mode")
-    print(toolbox_lib.warning + "Example of usage : launch_command.py Erec_Tuning -it 13 -z-sampling-mode 5-Positions -MC-SVN r1777")
+    print(toolbox.warning + "-interactive : Launch script in prompt")
+    print(toolbox.warning + "-debug : verbose-only mode")
+    print(toolbox.warning + "Example of usage : launch_command.py Erec_Tuning -it 13 -z-sampling-mode 5-Positions -MC-SVN r1777")
     sys.exit()
 
 
@@ -48,7 +49,7 @@ if len(sys.argv) == 1:
 script_subfolder = "LaunchCommand"
 script_names = command_arg_list[0]
 parmeters_dict["command"] = " ".join(command_arg_list)
-print(toolbox_lib.green_color + "Launching Command : " + toolbox_lib.reset_color + parmeters_dict["command"])
+print(toolbox.green_color + "Launching Command : " + toolbox.reset_color + parmeters_dict["command"])
 
 out_filename_item_list = list()
 for argument in command_arg_list:
@@ -63,20 +64,18 @@ for argument in command_arg_list:
             argument_str = ".." + argument_str[-20:-1]
         out_filename_item_list.append(argument_str)
 
-outfiles_base_name = toolbox_lib.get_now_time_string() + "_" + "_".join(out_filename_item_list)
+outfiles_base_name = toolbox.get_now_time_string() + "_" + "_".join(out_filename_item_list)
 if len(outfiles_base_name) > 200: outfiles_base_name = outfiles_base_name[0:200]
-print(toolbox_lib.gold_color + "Outfiles base name : " + toolbox_lib.reset_color + outfiles_base_name)
+print(toolbox.gold_color + "Outfiles base name : " + toolbox.reset_color + outfiles_base_name)
 
 this_python_script_path = os.path.dirname(os.path.realpath(__file__))
-WORK_DIR = os.environ.get("WORK_DIR")
-RESULTS_DIR = os.environ.get("WORK_DIR")
-launch_script_reservoir_path = this_python_script_path + "/LaunchCommand"
 execution_folder = RESULTS_DIR + "/" + script_names
-log_folder = WORK_DIR + "/Logs/" + command_arg_list[0] + "/"
+log_folder = JOBS_DIR + "/logs/" + command_arg_list[0] + "/"
+script_reservoir_folder = JOBS_DIR + "/scripts/" + command_arg_list[0] + "/"
 
-toolbox_lib.mkdir(launch_script_reservoir_path)
-toolbox_lib.mkdir(execution_folder)
-toolbox_lib.mkdir(log_folder)
+toolbox.mkdir(execution_folder)
+toolbox.mkdir(log_folder)
+toolbox.mkdir(script_reservoir_folder)
 
 
 #> Preparing launch script
@@ -100,15 +99,15 @@ lauch_script_string += "echo '**************************************************
 lauch_script_string += "echo COMPUTATION FINISHED\n"
 lauch_script_string += "echo '*******************************************************************'\n"
 
-launch_file = open(launch_script_reservoir_path + "/Script_" + outfiles_base_name + ".sh", 'w')
+launch_file = open(script_reservoir_folder + "/Script_" + outfiles_base_name + ".sh", 'w')
 launch_file.write(lauch_script_string)  # python will convert \n to os.linesep
 launch_file.close()  # you can omit in most cases as the destructor will call it
-print(toolbox_lib.green_color + "Launch script writen as : " + toolbox_lib.reset_color + launch_script_reservoir_path + "/Script_" + outfiles_base_name + ".sh")
+print(toolbox.green_color + "Launch script writen as : " + toolbox.reset_color + script_reservoir_folder + "/Script_" + outfiles_base_name + ".sh")
 
 #> Preparing job script
 job_command_arg_list = list()
 job_command_arg_list.append("qsub")
-job_command_arg_list.append("-l os=" + toolbox_lib.get_current_os())
+job_command_arg_list.append("-l os=" + toolbox.get_current_os())
 job_command_arg_list.append("-o " + log_folder + "/log_full_" + outfiles_base_name + ".log")
 job_command_arg_list.append("-e " + log_folder + "/log_full_" + outfiles_base_name + ".err")
 job_command_arg_list.append("-P P_t2k")
@@ -116,32 +115,21 @@ if parmeters_dict["multithread-support"]:
     job_command_arg_list.append("-pe multicores 8 -q mc_long")
 job_command_arg_list.append("-l xrootd=1,mysql=1,sps=1,hpss=1,ct=" + parmeters_dict["cpu_time"] + ",vmem=4G,fsize=20G")
 job_command_arg_list.append("-j y")
-job_command_arg_list.append(launch_script_reservoir_path + "/Script_" + outfiles_base_name + ".sh")
-
-
-
-# job_launch_command = "qsub -l os=" + toolbox_lib.get_current_os() + " "
-# job_launch_command += "-o " + log_folder + "/log_full_" + outfiles_base_name + ".log "
-# job_launch_command += "-e " + log_folder + "/log_full_" + outfiles_base_name + ".err "
-# job_launch_command += "-P P_stereo "
-# if parmeters_dict["multithread-support"]:
-#     job_launch_command += "-pe multicores 8 -q mc_long "
-# job_launch_command += "-l xrootd=1,sps=1,hpss=1,ct=" + parmeters_dict["cpu_time"] + ",vmem=4G,fsize=20G -j y "
-# job_launch_command += launch_script_reservoir_path + "/Script_" + outfiles_base_name + ".sh"
+job_command_arg_list.append(script_reservoir_folder + "/Script_" + outfiles_base_name + ".sh")
 
 
 job_launch_command = " ".join(job_command_arg_list)
 
-print(toolbox_lib.green_color + "Job command : " + toolbox_lib.reset_color + job_launch_command)
+print(toolbox.green_color + "Job command : " + toolbox.reset_color + job_launch_command)
 
 #> Launching Job
 if not debug:
     if mode == "job":
-        print(toolbox_lib.blue_color)
+        print(toolbox.blue_color)
         os.system(job_launch_command)
-        print(toolbox_lib.reset_color)
+        print(toolbox.reset_color)
     elif mode == "interactive":
         os.system(parmeters_dict["command"])
 
 # time.sleep(1)
-print(toolbox_lib.red_color + "************** Launch command ended. **************" + toolbox_lib.reset_color)
+print(toolbox.red_color + "************** Launch command ended. **************" + toolbox.reset_color)
