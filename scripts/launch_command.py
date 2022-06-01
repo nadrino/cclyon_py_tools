61dc27b77ed9fd23f17e758d5b42ef852902dddc
Adrien Blanchet <adrien.blanchet@live.fr>
2022-06-01 12:11:14 +0200

    upgrading launch_command.py with new cl reader

--- scripts/launch_command.py
+++ scripts/launch_command.py
@@ -4,196 +4,102 @@
 from GenericToolbox import IO as tIO
 from GenericToolbox import Old as tOld
 from GenericToolbox import Colors as tColors
+from GenericToolbox import CmdLineReader
 
 import sys
 import os
 
-useSbatch = True
 
 # Checking required env variables
 WORK_DIR = tIO.get_env_variable("WORK_DIR")
 JOBS_DIR = tIO.get_env_variable("JOBS_DIR")
 RESULTS_DIR = tIO.get_env_variable("RESULTS_DIR")
 
-parmeters_dict = dict()
-
-parmeters_dict["command"] = ""
-parmeters_dict["multithread-support"] = False
-parmeters_dict["cpu_time"] = "47:59:59"
-debug = False
-mode = "job"
-queue = "default"
-here = True  # always true -> for now
-command_arg_list = list()
-
-queues_info = tOld.get_queues_info()
-execution_folder = os.getcwd()
-nb_slots = 1
+executionFolder = os.getcwd()
 
 print(tColors.redColor + "************** Launch command starts **************" + tColors.resetColor)
 
-skip_next = True  # skip the first also
-reading_command = False
-for arg_id in range(len(sys.argv)):
+cl = CmdLineReader.CmdLineReader()
+cl.addOption("nCores", ["-mc"], description_="Trigger multi-core queue and specify nb threads/slots", nbExpectedArgs_=1)
+cl.addOption("debug", ["-debug"], description_="Trigger debug mode", nbExpectedArgs_=0)
+cl.addOption("interactive", ["-interactive"], description_="Launch the generated script in interactive mode", nbExpectedArgs_=0)
 
-    if skip_next:
-        skip_next = False
-        continue
+cl.keepTailArgs = True
 
-    if not reading_command:
-        if sys.argv[arg_id] == "-debug":
-            debug = True
-        elif sys.argv[arg_id] == "-interactive":
-            mode = "interactive"
-        # elif sys.argv[arg_id] == "-h":
-        #     here = True
-        #     execution_folder = os.getcwd()
-        elif sys.argv[arg_id] == "-mc":
-            parmeters_dict["multithread-support"] = True
-            queue = "mc_long"
-            nb_slots = int(sys.argv[arg_id + 1])
-            skip_next = True
-        elif sys.argv[arg_id] == "-q":
-            queue = sys.argv[arg_id + 1]
-            if queue not in queues_info:
-                print(tColors.error + "Unknown queue : " + queue)
-                for queue_name in queues_info:
-                    print(tColors.error + "  - " + queue_name)
-                exit(1)
-            else:
-                print(tColors.info + "Selected queue = " + queue)
-                for queue_info in queues_info[queue]:
-                    print(tColors.info + "  - " + queue_info + " : " + queues_info[queue][queue_info])
-            skip_next = True
-        else:
-            reading_command = True
-            command_arg_list.append(sys.argv[arg_id])
-    else:
-        command_arg_list.append(sys.argv[arg_id])
-        # this last option have to be in both the command and the python script
-        if sys.argv[arg_id] == "--multithread-support":
-            parmeters_dict["multithread-support"] = True
+cl.printConfigSummary()
+cl.readCommandLineArgs()
 
-if len(sys.argv) == 1 or len(command_arg_list) == 0:
-    print(tColors.warning + "The following option have to be put before the command")
-    print(tColors.warning + "-interactive : Launch script in prompt")
-    print(tColors.warning + "-debug : verbose-only mode")
-    print(tColors.warning + "-mc : Multicore preset (8cores + mc_long queue)")
-    print(tColors.warning + "-h : Execute the command here, from the current directory")
-    print(tColors.warning + "-q <name_of_the_queue> : Set specific queue")
-    print(tColors.warning + "-c <number_of_cores> : Specify the number of slots")
-    print(
-        tColors.warning + "Example of usage : launch_command.py -mc -c 2 Erec_Tuning -it 13 -z-sampling-mode 5-Positions -MC-SVN r1777")
-    sys.exit()
+if len(cl.trailArgList) == 0:
+    print(tColors.error + "No command specified to be launched")
+    exit(1)
+
 
 # > Parsing parameters
-script_subfolder = "LaunchCommand"
-script_names = command_arg_list[0].split("/")[-1]  # remove /bin/ etc...
-parmeters_dict["command"] = " ".join(command_arg_list)
-print(tColors.greenColor + "Launching Command : " + tColors.resetColor + parmeters_dict["command"])
-print(tColors.greenColor + "Launching from : " + tColors.redColor + execution_folder)
+cmdToJob = " ".join(cl.trailArgList)
+print(tColors.greenColor + "Launching Command : " + tColors.resetColor + cmdToJob)
+print(tColors.greenColor + "Launching from : " + tColors.redColor + executionFolder)
 
-out_filename_item_list = list()
-for argument in command_arg_list:
-    argument_str = str(argument)  # Copy string object
-    argument_str = argument_str.replace("@", "X")  # Replacing special characters
-    if argument_str[0] != '-' or (argument_str[0:2] == "--"):
-        if "/" in argument_str:
-            argument_str = argument_str.split("/")[-1]
-        if "." in argument_str:
-            argument_str = argument_str.split(".")[0]
-        if len(argument_str) > 20:
-            argument_str = ".." + argument_str[-20:-1]
-        out_filename_item_list.append(argument_str)
+# > Outfile names
+outFilesBaseName = tIO.getNowTimeString()
+for cmdArg in cl.trailArgList:
+    argElement = cmdArg  # Copy string object
+    argElement = argElement.replace("@", "X")  # Replacing special characters
+    if argElement[0] != '-' or (argElement[0:2] == "--"):
+        if "/" in argElement: argElement = argElement.split("/")[-1]
+        if "." in argElement: argElement = argElement.split(".")[0]
+        if len(argElement) > 20: argElement = ".." + argElement[-20:-1]
+        outFilesBaseName += "_" + argElement
 
-out_files_base_name = tIO.getNowTimeString() + "_" + "_".join(out_filename_item_list)
-if len(out_files_base_name) > 200: out_files_base_name = out_files_base_name[0:200]
-print(tColors.goldColor + "Output files base name : " + tColors.resetColor + out_files_base_name)
+if len(outFilesBaseName) > 200: outFilesBaseName = outFilesBaseName[0:200]
+print(tColors.goldColor + "Output files base name : " + tColors.resetColor + outFilesBaseName)
 
-this_python_script_path = os.path.dirname(os.path.realpath(__file__))
-log_folder = JOBS_DIR + "/logs/" + command_arg_list[0] + "/"
-script_reservoir_folder = JOBS_DIR + "/scripts/" + command_arg_list[0] + "/"
+logFolder = JOBS_DIR + "/logs/" + cl.trailArgList[0] + "/"
+scriptFolder = JOBS_DIR + "/scripts/" + cl.trailArgList[0] + "/"
 
-tIO.mkdir(log_folder)
-tIO.mkdir(script_reservoir_folder)
-
-if not here:
-    execution_folder = RESULTS_DIR + "/" + script_names
-    tIO.mkdir(execution_folder)
+tIO.mkdir(logFolder)
+tIO.mkdir(scriptFolder)
 
 # > Preparing launch script
-lauch_script_string = "#!/bin/bash\n"
-lauch_script_string += "\n"
-lauch_script_string += "echo cd " + execution_folder + "\n"
-lauch_script_string += "cd " + execution_folder + "\n"
-lauch_script_string += "\n"
-lauch_script_string += "echo '*******************************************************************'\n"
-lauch_script_string += "echo COMPUTATION BEGINS\n"
-lauch_script_string += "echo '*******************************************************************'\n"
-lauch_script_string += "echo " + parmeters_dict["command"] + "\n"
-lauch_script_string += parmeters_dict["command"] + " &> " + log_folder + "/log_" + out_files_base_name + ".log \n"
-lauch_script_string += "echo '*******************************************************************'\n"
-lauch_script_string += "echo COMPUTATION FINISHED\n"
-lauch_script_string += "echo '*******************************************************************'\n"
+cmdBashScript = "#!/bin/bash\n"
+cmdBashScript += "\n"
+cmdBashScript += "echo cd " + executionFolder + "\n"
+cmdBashScript += "cd " + executionFolder + "\n"
+cmdBashScript += "\n"
+cmdBashScript += "echo '*******************************************************************'\n"
+cmdBashScript += "echo COMPUTATION BEGINS\n"
+cmdBashScript += "echo '*******************************************************************'\n"
+cmdBashScript += "echo " + cmdToJob + "\n"
+cmdBashScript += cmdToJob + " &> " + logFolder + "/log_" + outFilesBaseName + ".log \n"
+cmdBashScript += "echo '*******************************************************************'\n"
+cmdBashScript += "echo COMPUTATION FINISHED\n"
+cmdBashScript += "echo '*******************************************************************'\n"
 
-launch_file = open(script_reservoir_folder + "/Script_" + out_files_base_name + ".sh", 'w')
-launch_file.write(lauch_script_string)  # python will convert \n to os.linesep
-launch_file.close()  # you can omit in most cases as the destructor will call it
-print(
-    tColors.greenColor + "Launch script writen as : " + tColors.resetColor + script_reservoir_folder + "/Script_" + out_files_base_name + ".sh")
+open(scriptFolder + "/Script_" + outFilesBaseName + ".sh", 'w').write(cmdBashScript)
+print(tColors.greenColor + "Launch script writen as : " + tColors.resetColor + scriptFolder + "/Script_" + outFilesBaseName + ".sh")
 
-job_launch_command = ""
-if useSbatch:
-    # > Preparing job script
-    job_command_arg_list = list()
-    job_command_arg_list.append("sbatch")
-    job_command_arg_list.append("-L sps")
-    job_command_arg_list.append("-n " + str(nb_slots))
-    job_command_arg_list.append("-o " + log_folder + "/log_full_" + out_files_base_name + ".log")
-    job_command_arg_list.append("-e " + log_folder + "/log_full_" + out_files_base_name + ".err")
-    job_command_arg_list.append("--mail-user=ablanche@lpnhe.in2p3.fr")
-    job_command_arg_list.append(script_reservoir_folder + "/Script_" + out_files_base_name + ".sh")
-    job_launch_command = " ".join(job_command_arg_list)
+# > Preparing job script
+jobSubArgList = list()
+jobSubArgList.append("sbatch")
+jobSubArgList.append("-L sps")
+if cl.isOptionTriggered("nbCores"):
+    jobSubArgList.append("-n " + str(cl.getOptionValues("nbCores")[0]))
+jobSubArgList.append("-o " + logFolder + "/log_full_" + outFilesBaseName + ".log")
+jobSubArgList.append("-e " + logFolder + "/log_full_" + outFilesBaseName + ".err")
+jobSubArgList.append("--mail-user=ablanche@lpnhe.in2p3.fr")
+jobSubArgList.append(scriptFolder + "/Script_" + outFilesBaseName + ".sh")
+jobSubCmd = " ".join(jobSubArgList)
 
-else:
-    resources_declaration = list()
-    # resources_declaration.append("xrootd=1")
-    # resources_declaration.append("mysql=1")
-    resources_declaration.append("sps=1")
-    # resources_declaration.append("hpss=1")
-    resources_declaration.append("ct=" + queues_info[queue]["max_cpu_time"])
-    # resources_declaration.append("vmem=" + queues_info[queue]["max_virtual"])
-    # resources_declaration.append("fsize=" + queues_info[queue]["max_file_size"])
-
-    # > Preparing job script
-    job_command_arg_list = list()
-    job_command_arg_list.append("qsub")
-    if queue != "default":
-        job_command_arg_list.append("-q " + queue)
-    job_command_arg_list.append("-l os=" + tIO.get_current_os())
-    job_command_arg_list.append("-o " + log_folder + "/log_full_" + out_files_base_name + ".log")
-    job_command_arg_list.append("-e " + log_folder + "/log_full_" + out_files_base_name + ".err")
-    job_command_arg_list.append("-P P_t2k")
-    if nb_slots != 1:
-        job_command_arg_list.append("-pe multicores " + str(nb_slots))
-    # job_command_arg_list.append("-l xrootd=1,mysql=1,sps=1,hpss=1,ct=" + parmeters_dict["cpu_time"] + ",vmem=4G,fsize=20G")
-    job_command_arg_list.append("-l " + ",".join(resources_declaration))
-    job_command_arg_list.append("-j y")
-    job_command_arg_list.append(script_reservoir_folder + "/Script_" + out_files_base_name + ".sh")
-
-    job_launch_command = " ".join(job_command_arg_list)
-
-print(tColors.greenColor + "Job command : " + tColors.resetColor + job_launch_command)
-print(tColors.greenColor + "Log path : " + tColors.resetColor + log_folder + "/log_" + out_files_base_name + ".log")
+print(tColors.greenColor + "Job command : " + tColors.resetColor + jobSubCmd)
+print(tColors.greenColor + "Log path : " + tColors.resetColor + logFolder + "/log_" + outFilesBaseName + ".log")
 
 # > Launching Job
-if not debug:
-    if mode == "job":
+if not cl.isOptionTriggered("debug"):
+    if cl.isOptionTriggered("interactive"):
+        os.system(cmdToJob)
+    else:
         print(tColors.blueColor)
-        os.system(job_launch_command)
+        os.system(jobSubCmd)
         print(tColors.resetColor)
-    elif mode == "interactive":
-        os.system(parmeters_dict["command"])
 
 # time.sleep(1)
 print(tColors.redColor + "************** Launch command ended. **************" + tColors.resetColor)
